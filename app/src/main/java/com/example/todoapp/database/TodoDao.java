86ce78aef399faabfc41591918f61c3476c8706e
bibash77261081 <cbibash20@gmail.com>
2023-05-08 23:11:59 +0545

    Data Access Object(DAO) for Todo created

---
+++ app/src/main/java/com/example/todoapp/database/TodoDao.java
@@ -0,0 +1,28 @@
+package com.example.todoapp.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TodoDao {
+
+    @Query("SELECT * FROM todos")
+    LiveData<List<Todo>> getAllTodos();
+
+    @Insert
+    void addTodo(Todo todo);
+
+    @Query("DELETE FROM todos")
+    void deleteALL();
+
+    @Update
+    void updateTodo(Todo todo);
+
+    @Query("DELETE FROM todos where id = :id")
+    void deleteById(int id);
+}
