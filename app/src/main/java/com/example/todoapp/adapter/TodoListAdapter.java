300aa5d54c0f9b50c7ec56d5edb930b632b31cd8
bibash77261081 <cbibash20@gmail.com>
2023-05-10 08:42:36 +0545

    Created an adapter to bridge todo data and todo-item(UI)

--- app/src/main/java/com/example/todoapp/adapter/TodoListAdapter.java
+++ app/src/main/java/com/example/todoapp/adapter/TodoListAdapter.java
@@ -1,4 +1,58 @@
 package com.example.todoapp.adapter;
 
-public class TodoListAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todoapp.R;
+import com.example.todoapp.database.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoViewHolder> {
+    private List<Todo> todos = new ArrayList<>();
+
+    public void setTodos(List<Todo> todos) {
+        this.todos = todos;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public TodoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_item, parent, false);
+        return new TodoViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TodoViewHolder holder, int position) {
+        Todo currentTodo = todos.get(position);
+        holder.txtTitle.setText(currentTodo.getTitle());
+        holder.txtDetail.setText(currentTodo.getDetail());
+        holder.txtDate.setText((CharSequence) currentTodo.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return todos.size();
+    }
+
+    static class TodoViewHolder extends RecyclerView.ViewHolder {
+        private TextView txtTitle;
+        private TextView txtDetail;
+        private TextView txtDate;
+
+        public TodoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtDetail = itemView.findViewById(R.id.txtDetail);
+            txtDate = itemView.findViewById(R.id.txtDate);
+        }
+    }
 }
+
