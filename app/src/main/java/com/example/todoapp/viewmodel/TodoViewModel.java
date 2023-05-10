7d63a0ea570032d949aa5d0c34f08f54a4c20171
bibash77261081 <cbibash20@gmail.com>
2023-05-10 10:04:05 +0545

    delete and update action on todo detail

--- app/src/main/java/com/example/todoapp/viewmodel/TodoViewModel.java
+++ app/src/main/java/com/example/todoapp/viewmodel/TodoViewModel.java
@@ -1,16 +1,21 @@
 package com.example.todoapp.viewmodel;
 
 import android.app.Application;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.room.TypeConverters;
 
+import com.example.todoapp.database.DateTypeConverter;
 import com.example.todoapp.database.Todo;
 import com.example.todoapp.repository.TodoRepository;
 
+import java.util.Date;
 import java.util.List;
 
+@TypeConverters(DateTypeConverter.class)
 public class TodoViewModel extends AndroidViewModel {
     private TodoRepository todoRepository;
     private LiveData<List<Todo>> allTodos;
@@ -25,15 +30,26 @@ public class TodoViewModel extends AndroidViewModel {
         return allTodos;
     }
 
-    public void insert(Todo todo) {
+    public void insert() {
+        Todo todo = new Todo();
         todoRepository.insert(todo);
     }
 
-    public void update(Todo todo) {
+    public void update() {
+        Todo todo = new Todo();
         todoRepository.update(todo);
     }
 
-    public void delete(Todo todo) {
+    public void updateTodoTitleAndDetail(String title, String detail) {
+        Todo todo = new Todo();
+        todo.setTitle(title);
+        todo.setDetail(detail);
+//        todo.setDate(date);
+        todoRepository.update(todo);
+    }
+
+    public void delete() {
+        Todo todo = new Todo();
         todoRepository.delete(todo);
     }
 }
