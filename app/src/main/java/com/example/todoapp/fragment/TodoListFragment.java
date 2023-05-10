f6a69da26f5a1607e7b1332f7c5cb64d95209582
bibash77261081 <cbibash20@gmail.com>
2023-05-10 09:08:47 +0545

    logic for todo list created

--- app/src/main/java/com/example/todoapp/fragment/TodoListFragment.java
+++ app/src/main/java/com/example/todoapp/fragment/TodoListFragment.java
@@ -1,66 +1,54 @@
 package com.example.todoapp.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.todoapp.R;
+import com.example.todoapp.adapter.TodoListAdapter;
+import com.example.todoapp.database.Todo;
+import com.example.todoapp.viewmodel.TodoViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TodoListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
 public class TodoListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TodoViewModel todoViewModel;
+    private TodoListAdapter todoListAdapter;
 
     public TodoListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TodoListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TodoListFragment newInstance(String param1, String param2) {
-        TodoListFragment fragment = new TodoListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        todoViewModel = new ViewModelProvider(this).get(TodoViewModel.class);
+        todoListAdapter = new TodoListAdapter();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_todo_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        recyclerView.setAdapter(todoListAdapter);
+
+        todoViewModel.getAllTodos().observe(getViewLifecycleOwner(), new Observer<List<Todo>>() {
+            @Override
+            public void onChanged(List<Todo> todos) {
+                todoListAdapter.setTodos(todos);
+            }
+        });
+
+        return view;
     }
 }
