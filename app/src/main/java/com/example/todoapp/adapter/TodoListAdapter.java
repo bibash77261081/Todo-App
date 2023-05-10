47aaad6a9bf8bc1e9caaa62313cdd5c15dcae3da
bibash77261081 <cbibash20@gmail.com>
2023-05-10 10:51:18 +0545

    navigation file added so that after clicking on todo item it will navigate to todo detail

--- app/src/main/java/com/example/todoapp/adapter/TodoListAdapter.java
+++ app/src/main/java/com/example/todoapp/adapter/TodoListAdapter.java
@@ -15,7 +15,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoViewHolder> {
-    private List<Todo> todos = new ArrayList<>();
+    private List<Todo> todos;
+    private OnTodoItemClickListener itemClickListener;
+
+    public TodoListAdapter(OnTodoItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+        todos = new ArrayList<>();
+    }
 
     public void setTodos(List<Todo> todos) {
         this.todos = todos;
@@ -25,16 +31,15 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoVi
     @NonNull
     @Override
     public TodoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_item, parent, false);
-        return new TodoViewHolder(itemView);
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.todo_item, parent, false);
+        return new TodoViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TodoViewHolder holder, int position) {
-        Todo currentTodo = todos.get(position);
-        holder.txtTitle.setText(currentTodo.getTitle());
-        holder.txtDetail.setText(currentTodo.getDetail());
-        holder.txtDate.setText((CharSequence) currentTodo.getDate());
+        Todo todo = todos.get(position);
+        holder.bind(todo);
     }
 
     @Override
@@ -42,17 +47,30 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoVi
         return todos.size();
     }
 
-    static class TodoViewHolder extends RecyclerView.ViewHolder {
+    class TodoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView txtTitle;
-        private TextView txtDetail;
-        private TextView txtDate;
 
-        public TodoViewHolder(@NonNull View itemView) {
+        TodoViewHolder(@NonNull View itemView) {
             super(itemView);
             txtTitle = itemView.findViewById(R.id.txtTitle);
-            txtDetail = itemView.findViewById(R.id.txtDetail);
-            txtDate = itemView.findViewById(R.id.txtDate);
+            itemView.setOnClickListener(this);
+        }
+
+        void bind(Todo todo) {
+            txtTitle.setText(todo.getTitle());
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                Todo todo = todos.get(position);
+                itemClickListener.onTodoItemClick(todo);
+            }
         }
     }
-}
 
+    public interface OnTodoItemClickListener {
+        void onTodoItemClick(Todo todo);
+    }
+}
